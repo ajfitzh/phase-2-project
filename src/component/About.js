0f0455ea078b45b4b08c23039631d0cd0787e11f
Austin J FitzHugh <austin.j.fitzhugh@gmail.com>
2022-01-04 16:13:27 -0500

    added new carousel

--- src/component/About.js
+++ src/component/About.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect } from 'react'
 import ReviewForm from "./ReviewForm"
 import Review from "./Review"
 import mikedog from "./assets/mikedog.png"
+import mikewife from "./assets/mikewife.jpg"
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
@@ -11,6 +12,10 @@ import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import { Grid } from "@material-ui/core"
+import "./About.css"
+import Carousel from 'react-material-ui-carousel'
+import { Paper } from '@mui/material'
+
 
 const useStyles = makeStyles({
     gridContainer: {
@@ -18,6 +23,17 @@ const useStyles = makeStyles({
         paddingRight: '20px'
     }
 })
+
+var items = [
+    {
+        name: "Random Name #1",
+        imageURL: "https://www.familyhandyman.com/wp-content/uploads/2017/06/FH06SEP_MOTORC_01-2.jpg"
+    },
+    {
+        name: "Random Name #2",
+        imageURL: "http://ridermagazine.com/wp-content/uploads/2018/07/LEADIMG_3598.jpg"
+    }
+]
 
 const About = props => {
     const [reviews, setReviews] = useState([])
@@ -32,11 +48,32 @@ const About = props => {
       function onAddReview(newReview){
           setReviews([...reviews,newReview])
       }
+
+      function Item(props)
+{
+    return (
+        <Paper>
+            <p>{props.item.name}</p>
+            <img width="60%" height="400vw" src={props.item.imageURL}/>
+        </Paper>
+    )
+}
     return (
         <div>
             
-            <Typography align="center" gutterBottom variant="h2">Our Story</Typography>
-           
+            <Typography align="center" gutterBottom variant="h4">Our Story</Typography>
+           <Card align="center">
+                <Carousel>
+                    {
+                        items.map( (item, i) => <Item key={i} item={item} /> )
+                    }
+                </Carousel>
+               <div className="font-link">
+               <h1> Founded in 2021, built upon decades of experience <br></br> and a brand-new workshop.<br></br> We guarantee <br></br> craftsmanship, attention to detail, and innovation. <br></br>
+                 -Mike M.
+                </h1>
+                </div>
+            </Card>
             <Typography align="center" gutterBottom variant="h2">Our Customer Stories</Typography>
            <Grid container justifyContent="center" container spacing={12} row spacing={12}>
            {reviews.map(review => <Review key={review.id} text={review.text}/>)} 
