df2c59a2d658e4029bc76cc5057e3006583c27a9
Ahmet Uysaler <ahmetsenih@gmail.com>
2022-01-06 19:06:34 -0600

    finished sort

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -10,9 +10,6 @@ const useStyles = makeStyles({
           paddingRight: '20px',
       }
   })
-function sortByPrice() {
-    
-}
 function Bikes() {
     const [bikes, setBikes] = useState([]);
     useEffect(() => {
@@ -24,18 +21,68 @@ function Bikes() {
       }, [])
       return bikes;
 }
+function sortByPriceHighToLow() {
+    function compare( a, b ) {
+        if ( a.price > b.price ){
+          return -1;
+        }
+        if ( a.price < b.price ){
+          return 1;
+        }
+        return 0;
+      }
+    let bikes = Bikes().sort(compare);
+    return bikes
+}
+function sortByPriceLowToHigh() {
+    function compare( a, b ) {
+        if ( a.price < b.price ){
+          return -1;
+        }
+        if ( a.price > b.price ){
+          return 1;
+        }
+        return 0;
+      }
+    let bikes = Bikes().sort(compare);
+    return bikes
+}
+
+function Sort({sort, setSort}) {
+    return (
+        <>
+        <label>Sort by:</label>
+        <select onChange={(e) => setSort(e.target.value)}>
+        <option value="default">Choose an option below</option>    
+        <option value="low-to-high">Price Low to High</option>
+        <option value="high-to-low">Price High to Low</option>
+        </select>
+        </>
+    )
+}
+function sorter(sort) {
+        if(sort === "low-to-high") 
+             {   
+               return sortByPriceLowToHigh();       
+             } 
+            else if(sort === "high-to-low")
+             {
+               return sortByPriceHighToLow();
+            }
+            else
+            {
+                return Bikes();
+            }
+    } 
 const Projects = props => {
 const classes = useStyles();
-
+const [sort, setSort] = useState("");
+const bikes = sorter(sort);
   return (
         <>
-            <label>Sort by Price:</label>
-            <select onChange={Bikes()}>
-            <option value="low-to-high">Low to High</option>
-            <option value="high-to-low">High to Low</option>
-            </select>
+           <Sort sort = {sort} setSort = {setSort}></Sort>
             <Grid container spacing={2}>
-                {Bikes().map((bike) => {
+                {bikes.map((bike) => {
                     return (
                        <Grid item xs={12} sm={6} md={4}>
                            <Project bike = {bike}/>
